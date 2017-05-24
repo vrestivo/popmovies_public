9b22790b5a9aedf236337a7bc0bca651e74f6c2c
Vladimir Restivo <vladimir.restivo@gmail.com>
2017-05-24 13:59:48 -0700

    refactor: implement GuestureDetector

--- app/src/main/java/com/example/android/popmoviesstage2/MySwipeDismissListener.java
+++ app/src/main/java/com/example/android/popmoviesstage2/MySwipeDismissListener.java
@@ -2,6 +2,7 @@ package com.example.android.popmoviesstage2;
 
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
@@ -9,11 +10,12 @@ import android.view.ViewConfiguration;
 
 import org.jetbrains.annotations.NotNull;
 
+
 /**
  * Created by devbox on 5/24/17.
  */
 
-public class MySwipeDismissListener implements View.OnTouchListener {
+public class MySwipeDismissListener implements GestureDetector.OnGestureListener {
 
     private final String LOG_TAG = this.getClass().getSimpleName();
 
@@ -48,10 +50,45 @@ public class MySwipeDismissListener implements View.OnTouchListener {
         mSlop = viewCofig.getScaledTouchSlop();
 
         //TODO setup callbacks
+    }
 
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.v(LOG_TAG, "_in onDown");
+        
+        //best practice
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
 
     }
 
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.v(LOG_TAG, "_in onFling: " + e1.toString() +"/" + e2.toString() + " "
+                + velocityX + "/" + velocityY);
+
+        return true;
+    }
+
+    /*
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
@@ -63,8 +100,10 @@ public class MySwipeDismissListener implements View.OnTouchListener {
                 Log.v(LOG_TAG, "_in onTouch DOWN");
                 //TODO add event to motion tracker
                 //TODO get initial coordinates
-                //
 
+                //returning false will not trigger
+                //further action events
+                return super.on;
             }
 
             case MotionEvent.ACTION_UP: {
@@ -89,4 +128,8 @@ public class MySwipeDismissListener implements View.OnTouchListener {
 
         return false;
     }
+    */
+
+
+
 }
